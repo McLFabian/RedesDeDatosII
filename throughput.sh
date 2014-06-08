9be51134bf79f06a771d466494583411c548eeaf
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2014-06-08 16:55:27 -0400

    Throughput - pseudocodigo

--- throughput.sh
+++ throughput.sh
@@ -4,9 +4,15 @@ cat file.csv | grep "192.168.23.32 80 192.168.23.172 49567" > server_to_cliente.
 rm -rfv file.csv
 bits_trasmitidos=0
 paquetes_enviados=0
+tiempo=0
 
 while read line
 do
-	bits=
+	bits=$(awk ....)
+	let bits_transmitidos=bits_transmitidos+bits
+	let paquetes_enviados=paquetes_enviados+1
+	
 done<server_to_cliente.tmp
+
+thoughput=$(echo "$paquetes_enviados / $tiempo"|bc)
 
