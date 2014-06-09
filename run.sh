da3a8f68491936b15c616b291078b10417dfe125
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2014-06-09 11:57:23 -0400

    UPDATE RUN

--- run.sh
+++ run.sh
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 echo "">log.txt
 chmod +x *
 echo "Calculanto RTT promedio..."
 ./rtt.sh
 echo "Calculando Throughput..."
 ./throughput.sh
-=======
-echo "Desifrando paquetes..."
-chmod +x TShark.sh
-./TShark.sh
-echo "Calculanto RTT promedio..."
-./RTT.sh
-
->>>>>>> 134cd6bdfb5a67262e84fe0a10c210e3d4699607
